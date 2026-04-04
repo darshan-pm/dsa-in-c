01e3c51ad012e4e63f0ea66949034bb6f0aec7c1
Darshan <darshan200713@gmail.com>
2026-04-04 16:48:49 +0530

    added tail recursion problem1

---
+++ tail_recursion_problem1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void fun(int n){
+    if(n==0)
+        return;
+        else
+         printf("%d ", n);
+        return fun(n - 1);
+    }
+    int main(){
+        fun(5);
+        return 0;
+    }
